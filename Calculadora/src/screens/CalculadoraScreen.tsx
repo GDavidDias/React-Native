b8087774eedb7c5a7af20e9eab9d4e900a974372
GDavidDias <guillermo.david.dias@gmail.com>
2023-12-05 12:00:00 -0300

    Calculadora: hooks useCalculadora

--- Calculadora/src/screens/CalculadoraScreen.tsx
+++ Calculadora/src/screens/CalculadoraScreen.tsx
@@ -2,123 +2,133 @@ import React, { useEffect, useReducer, useRef, useState } from 'react'
 import { Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { BotonCalculadora } from '../components/BotonCalculadora'
+import { useCalculadora } from '../hooks/useCalculadora';
 
 export const CalculadoraScreen = () => {
 
-  const [numero, setNumero] = useState('0');
-  const [numeroAnterior, setNumeroAnterior] = useState('0');
+  const{numero,numeroAnterior,limpiar,
+    armarNumero,
+    del,
+    positivoNegativo,
+    btnSumar,
+    btnRestar,
+    btnMultiplicar,
+    btnDividir,
+    calcular} = useCalculadora();
 
-  enum Operadores{
-    sumar,restar,multiplicar,dividir
-  };
+  // const [numero, setNumero] = useState('0');
+  // const [numeroAnterior, setNumeroAnterior] = useState('0');
 
-  const ultimaOperacion = useRef<Operadores>()
+  // enum Operadores{
+  //   sumar,restar,multiplicar,dividir
+  // };
 
-  const limpiar = () =>{
-    setNumero('0');
-    setNumeroAnterior('0');
-  };
+  // const ultimaOperacion = useRef<Operadores>()
 
-  const armarNumero = (numeroTexto:string) =>{
-    //No aceptar doble punto
-    if(numero.includes('.') && numeroTexto === '.') return;
+  // const limpiar = () =>{
+  //   setNumero('0');
+  //   setNumeroAnterior('0');
+  // };
+
+  // const armarNumero = (numeroTexto:string) =>{
+  //   //No aceptar doble punto
+  //   if(numero.includes('.') && numeroTexto === '.') return;
     
-    if(numero.startsWith('0') || numero.startsWith('-0')){
-      //Punto decimal
-      if(numeroTexto === '.'){
-        setNumero(numero + numeroTexto)
+  //   if(numero.startsWith('0') || numero.startsWith('-0')){
+  //     //Punto decimal
+  //     if(numeroTexto === '.'){
+  //       setNumero(numero + numeroTexto)
         
-        //Evaluar si es otro cero, y hay un punto
-      }else if(numeroTexto==='0' && numero.includes('.')){
-        setNumero(numero + numeroTexto)
+  //       //Evaluar si es otro cero, y hay un punto
+  //     }else if(numeroTexto==='0' && numero.includes('.')){
+  //       setNumero(numero + numeroTexto)
 
-        //Evaluar si es diferente de cero y no tiene un punto
-      }else if(numeroTexto!=='0' && !numero.includes('.')){
-        setNumero(numeroTexto)
+  //       //Evaluar si es diferente de cero y no tiene un punto
+  //     }else if(numeroTexto!=='0' && !numero.includes('.')){
+  //       setNumero(numeroTexto)
 
-      }else if(numeroTexto==='0' && !numero.includes('.')){
-        setNumero(numero);
-      }else{
-        setNumero(numero + numeroTexto)
-      }
+  //     }else if(numeroTexto==='0' && !numero.includes('.')){
+  //       setNumero(numero);
+  //     }else{
+  //       setNumero(numero + numeroTexto)
+  //     }
 
-    }else{
-      setNumero(numero+numeroTexto)
-    }
-  };
+  //   }else{
+  //     setNumero(numero+numeroTexto)
+  //   }
+  // };
 
-  const del=()=>{
-    console.log(numero.length)
-    if(numero.length>=1){
-      if(numero.length===1 && numero ==='0') return;
-      if(numero.length===2 && numero.includes('-')){
-        setNumero('0')
-        return
-      }
-      const newNumero = numero.slice(0,-1)
-      setNumero(newNumero)
-    }else{
-      setNumero('0')
-    }
-  }
+  // const del=()=>{
+  //   console.log(numero.length)
+  //   if(numero.length>=1){
+  //     if(numero.length===1 && numero ==='0') return;
+  //     if(numero.length===2 && numero.includes('-')){
+  //       setNumero('0')
+  //       return
+  //     }
+  //     const newNumero = numero.slice(0,-1)
+  //     setNumero(newNumero)
+  //   }else{
+  //     setNumero('0')
+  //   }
+  // }
 
-  const positivoNegativo = () =>{
-    if(numero.includes('-')){
-      setNumero(numero.replace('-', ''))
-    }else{
-      setNumero('-' + numero)
-    }
-  }
+  // const positivoNegativo = () =>{
+  //   if(numero.includes('-')){
+  //     setNumero(numero.replace('-', ''))
+  //   }else{
+  //     setNumero('-' + numero)
+  //   }
+  // }
 
-  const cambioNumAnterior = ()=>{
-    if(numero.endsWith('.')){
-      setNumeroAnterior(numero.slice(0,-1))
-    }else{
-      setNumeroAnterior(numero)
-    }
-    setNumero('0')
-  }
+  // const cambioNumAnterior = ()=>{
+  //   if(numero.endsWith('.')){
+  //     setNumeroAnterior(numero.slice(0,-1))
+  //   }else{
+  //     setNumeroAnterior(numero)
+  //   }
+  //   setNumero('0')
+  // }
   
-  const btnSumar = () =>{
-    cambioNumAnterior();
-    ultimaOperacion.current=Operadores.sumar;
-  }
-  const btnRestar = () =>{
-    cambioNumAnterior();
-    ultimaOperacion.current=Operadores.restar;
-  }
-  const btnMultiplicar = () =>{
-    cambioNumAnterior();
-    ultimaOperacion.current=Operadores.multiplicar;
-  }
-  const btnDividir = () =>{
-    cambioNumAnterior();
-    ultimaOperacion.current=Operadores.dividir;
-  }
+  // const btnSumar = () =>{
+  //   cambioNumAnterior();
+  //   ultimaOperacion.current=Operadores.sumar;
+  // }
+  // const btnRestar = () =>{
+  //   cambioNumAnterior();
+  //   ultimaOperacion.current=Operadores.restar;
+  // }
+  // const btnMultiplicar = () =>{
+  //   cambioNumAnterior();
+  //   ultimaOperacion.current=Operadores.multiplicar;
+  // }
+  // const btnDividir = () =>{
+  //   cambioNumAnterior();
+  //   ultimaOperacion.current=Operadores.dividir;
+  // }
   
-  const calcular = ()=>{
-    const n1 = Number(numero)
-    const n2 = Number(numeroAnterior)
+  // const calcular = ()=>{
+  //   const n1 = Number(numero)
+  //   const n2 = Number(numeroAnterior)
 
-    switch (ultimaOperacion.current) {
-      case Operadores.sumar:
-        setNumero(`${n1+n2}`)
-        break;
-      case Operadores.restar:
-        setNumero(`${n2-n1}`)
-        break;
-      case Operadores.multiplicar:
-        setNumero(`${n1*n2}`)
-        break;
-      case Operadores.dividir:
-        setNumero(`${n2/n1}`)
-        break;
-    
-      default:
-        break;
-    }
-    setNumeroAnterior('0')
-  }
+  //   switch (ultimaOperacion.current) {
+  //     case Operadores.sumar:
+  //       setNumero(`${n1+n2}`)
+  //       break;
+  //     case Operadores.restar:
+  //       setNumero(`${n2-n1}`)
+  //       break;
+  //     case Operadores.multiplicar:
+  //       setNumero(`${n1*n2}`)
+  //       break;
+  //     case Operadores.dividir:
+  //       if(n1!==0){
+  //         setNumero(`${n2/n1}`)
+  //       }
+  //       break;
+  //   }
+  //   setNumeroAnterior('0')
+  // }
 
 
 
